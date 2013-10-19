b11557c58dc2c47adf364617f493bd095d2639a4
Pierre Rudloff <contact@rudloff.pro>
2013-10-19 12:13:35 +0000

    Truc panneaux

--- directions.php
+++ directions.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Get coords from KML file and route from MapQuest API
+ * 
+ * PHP Version 5.4.4
+ * 
+ * @category API
+ * @package  Panneaux
+ * @author   Pierre Rudloff <contact@rudloff.pro>
+ * @license  LGPL https://www.gnu.org/licenses/lgpl.html
+ * @link     https://rudloff.pro/
+ * */
 $dom = new DOMDocument();
 $dom->load('liste.kml');
 $places = $dom->getElementsByTagName('Placemark');
@@ -6,15 +17,24 @@ $waypoints = array();
 foreach ($places as $place) {
     $name = $place->getElementsByTagName('name')->item(0)->textContent;
     if (strpos($name, $_GET['quartier'])) {
-        $name = explode('|', $name);
-        $coords = $place->getElementsByTagName('Point')->item(0)->getElementsByTagName('coordinates')->item(0)->textContent;
+        $coords = $place->getElementsByTagName('Point')
+            ->item(0)->getElementsByTagName('coordinates')
+            ->item(0)->textContent;
         $coords = str_replace(',0.000000', '', $coords);
         $coords = split(',', $coords);
-        $locations[]= array('latLng'=>array('lat'=>$coords[1], 'lng'=>$coords[0]), 'title'=>$name[0]);
+        $locations[]= array('latLng'=>array('lat'=>$coords[1], 'lng'=>$coords[0]));
     }
 }
-$json=(json_encode(array('locations'=>$locations, 'routeType'=>'bicycle', 'locale'=>'fr', 'narrativeType'=>'none', 'fullShape'=>true)));
-$route=(json_decode(file_get_contents('http://open.mapquestapi.com/directions/v2/optimizedRoute?json='.urlencode($json)))->route);
+$json=json_encode(
+    array('locations'=>$locations, 'routeType'=>'bicycle', 'locale'=>'fr',
+    'narrativeType'=>'none', 'fullShape'=>true)
+);
+$route=json_decode(
+    file_get_contents(
+        'http://open.mapquestapi.com/directions/v2/optimizedRoute?json='.
+        urlencode($json)
+    )
+)->route;
 if (isset($route->shape)) {
     foreach ($route->shape->shapePoints as $key => $value) {
         if ($key % 2 == 0) {
@@ -24,5 +44,9 @@ if (isset($route->shape)) {
 } else {
     $points =array();
 }
-print(json_encode(array('polyline'=>$points, 'points'=>$locations)));
+$orderedLocations = array();
+foreach ($route->locations as $point) {
+    $orderedLocations[]= array('latLng'=>$point->latLng, 'title'=>$point->street);
+} 
+print(json_encode(array('polyline'=>$points, 'points'=>$orderedLocations)));
 ?>
--- script.js
+++ script.js
@@ -7,21 +7,28 @@ var addPoints = function (point) {
 };
 var addPolyline = function (data) {
     'use strict';
-    var polyline = L.polyline(data.polyline, {color: 'red'});
+    var polyline = L.polyline(data.polyline, {color: 'red'}), gMap = 'https://maps.google.fr/maps?hl=fr&lci=bike&dirflg=b&daddr=', i;
     polylines.addLayer(polyline).addTo(map);
     map.fitBounds(polyline.getBounds());
     data.points.forEach(addPoints);
     markers.addTo(map);
-    //$('html, body').animate({scrollTop:0}, 'fast');
+    for (i = 0; i < data.points.length - 1; i += 1) {
+        gMap += data.points[i].latLng.lat + ',' + data.points[i].latLng.lng + '+to:';
+    }
+    gMap += data.points[data.points.length - 1].latLng.lat + ',' + data.points[data.points.length - 1].latLng.lng;
+    $('#gMaps').attr('href', gMap).show();
+    $('html, body').animate({scrollTop: $('#map').position().top}, 'fast');
     $.mobile.loading('hide');
 };
 
-var loadMap = function () {
+var loadMap = function (e) {
     'use strict';
+    e.preventDefault();
     markers.clearLayers();
     polylines.clearLayers();
     $.mobile.loading('show', {text: 'Calcul du tracé', textVisible: true});
     $.getJSON('directions.php?quartier=' + this.textContent, addPolyline);
+    return false;
 };
 
 var init = function () {
@@ -30,6 +37,8 @@ var init = function () {
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    $('.button').click(loadMap);
+    $('.button').on('vclick', loadMap);
+    $('#map_wrapper').append('<a data-iconpos="right" data-icon="forward" id="gMaps" data-role="button" target="_blank">Open in Google Maps</a>').trigger('create');
+    $('#gMaps').hide();
 };
 $(window).ready(init);
